f7448a08c1f806c7eeda3a9b6cbee7d14fd72fc8
wmt-harshs <harshs@webmob.tech>
2022-02-11 11:35:23 +0530

    Guess number game, input by computer

--- Guess_number/By_Computer/guess.py
+++ Guess_number/By_Computer/guess.py
@@ -6,7 +6,7 @@ def guess(x):
     randum_number = random.randint(1,x)
     guess = 0
     while guess != randum_number:
-        guess = int(input("Enter a guess nnumber:- "))
+        guess = int(input(f"Enter a guess nnumber between 1 and {x}:- "))
         if guess < randum_number:
             print("Sorry guess again,too low number.")
         elif guess > randum_number:
