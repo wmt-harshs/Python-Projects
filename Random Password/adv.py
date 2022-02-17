3e828b260c52e0f39ecd305a7c128990e71663e2
wmt-harshs <harshs@webmob.tech>
2022-02-17 16:24:49 +0530

    Modify file

--- Random Password/adv.py
+++ Random Password/adv.py
@@ -8,23 +8,28 @@ if __name__ == "__main__":
     number = list(string.digits)
     simbol = list(string.punctuation)
 
+    how_many = int(input("Enter total password you want:- "))
     up = int(input("Enter the uppers case letter you want:- "))
     low = int(input("Enter the lowers case letter you want:- "))
     num = int(input("Enter the numbers letter you want:- "))
     sim = int(input("Enter the simbols you want:- "))
 
-    pass_up = random.sample(upeer_char, k = up)
-    pass_low = random.sample(lower_char, k = low)
-    pass_num = random.sample(number, k = num)
-    pass_sim = random.sample(simbol, k = sim)
+    while how_many !=0:
+        pass_up = random.sample(upeer_char, k = up)
+        pass_low = random.sample(lower_char, k = low)
+        pass_num = random.sample(number, k = num)
+        pass_sim = random.sample(simbol, k = sim)
 
-    password = []   
-    password.extend(list(pass_up))
-    password.extend(list(pass_low))
-    password.extend(list(pass_num))
-    password.extend(list(pass_sim))
+        password = []   
+        password.extend(list(pass_up))
+        password.extend(list(pass_low))
+        password.extend(list(pass_num))
+        password.extend(list(pass_sim))
 
-    #random.shuffle(password, k=len(password))
-    random.shuffle(password)
-    print("Your password is:- " + "".join(password))
-    
+        #random.shuffle(password, k=len(password))
+        random.shuffle(password)
+               
+        pn = how_many * 2 - how_many   
+        print("Your " + str(pn) + " password is:- " + "".join(password))
+        pn -= 1
+        how_many -= 1
